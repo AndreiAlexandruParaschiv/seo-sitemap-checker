ac0a62d8939f2e4f1adfff14e7fd095fc063f70b
Andrei Paraschiv <paraschi@adobe.com>
2025-02-03 23:51:45 +0200

    feat: add percentages calculations for robots tags

--- metaconfig.js
+++ metaconfig.js
@@ -1,6 +1,24 @@
 // config.js
 const sitemapUrls = [
     // Fill in the URLs of the sitemaps you want to check
+    'https://www.bamboohr.com/sitemap.xml',
+    'https://www.bamboohr.com/blog/sitemap.xml',
+    'https://www.bamboohr.com/resources/hr-glossary/sitemap.xml' ,
+    'https://www.bamboohr.com/resources/sitemap.xml' ,
+    'https://www.bamboohr.com/resources/guides/sitemap.xml' ,
+    'https://www.bamboohr.com/webinars/sitemap.xml' ,
+    'https://www.bamboohr.com/job-description/sitemap.xml' ,
+    'https://www.bamboohr.com/virtual-ebook/sitemap.xml' ,
+    'https://www.bamboohr.com/sitemap-solutions.xml' ,
+    'https://www.bamboohr.com/sitemap-industry.xml' ,
+    'https://www.bamboohr.com/sitemap-virtual.xml' ,
+    'https://www.bamboohr.com/sitemap-who-we-serve.xml' ,
+    'https://www.bamboohr.com/product-updates/sitemap.xml',
+    'https://www.bamboohr.com/hr-software/sitemap.xml' ,
+    'https://www.bamboohr.com/integrations/sitemap.xml',
+    'https://www.bamboohr.com/sitemap-about-bamboohr.xml' ,
+    'https://www.bamboohr.com/careers/sitemap.xml',
+    'https://www.bamboohr.com/customers/sitemap.xml'
 ];
 
 module.exports = {
